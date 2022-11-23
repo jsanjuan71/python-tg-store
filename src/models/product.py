7910d9a5453ce64776b43a7492bdb33ef7799804
Julio Sanjuan <julio.sanjuan@konfio.mx>
2022-11-23 07:21:25 -0600

    wip Product definition

--- src/models/product.py
+++ src/models/product.py
@@ -7,6 +7,7 @@ class Product:
     thumbnail_url = None
     condition = None
 
+
     def __init__(self, id, title, price, currency, available_quantity, thumbnail, condition):
         self.id = id
         self.title = title
@@ -15,6 +16,33 @@ class Product:
         self.stock = available_quantity
         self.thumbnail = thumbnail
         self.condition = condition
+
+    def retrieveTax(self):
+        return self.price * 0.16
+
+    
+    def setPrecio(self, precio):
+        if precio < 0:
+            return None
+
+    
     
     def __str__(self):
-        return f"{self.id}: {self.title}"
+        return f"Soy {self.title} con id: {self.id}."
+
+  
+"""
+celular = Product(1, "Samsung Galaxy", 2000, "MXN", 10, "url", "nuevo")
+
+
+laptop = Product(2, "macbook pro", 15000, "MXN", 5, "url", "nuevo")
+laptop.retrieveTax()
+iva = celular.retrieveTax()
+
+celular.price = 20
+
+celular.setPrecio(20)
+
+print(celular)
+
+"""
