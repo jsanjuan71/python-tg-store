9dff75869c78859d88fabae6327e1e8649d3b22b
Julio Sanjuan <julio.sanjuan@konfio.mx>
2022-12-01 17:47:22 -0600

    force to not verify on get from api

--- src/services/products.py
+++ src/services/products.py
@@ -13,10 +13,11 @@ class ProductService:
         print("Product service")
 
     def searchByKeyword(self, keyword):
+
         cached = cache.get(keyword) #intentar obtener desde caché
         if cached == None: # No existe en caché, invocar al API de ML
             print("Getting from ML API")
-            api_response = requests.get(f"{self.API_URL}/search?q={keyword}")
+            api_response = requests.get(f"{self.API_URL}/search?q={keyword}", verify=False)
             if(api_response.status_code == 200):
                 list = []
                 payload = api_response.json()
